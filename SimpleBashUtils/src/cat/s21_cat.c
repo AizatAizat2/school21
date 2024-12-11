7d35c62ec82c6239e8afc7453bf24ca5eaf41ddf
AizatAizat2 <156372828+AizatAizat2@users.noreply.github.com>
2024-12-11 17:33:05 +0300

    Add files via upload

---
+++ SimpleBashUtils/src/cat/s21_cat.c
@@ -0,0 +1,176 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct options {
+  int b, e, v, n, s, t;
+} opt;
+
+void parser(int argc, char *argv[], opt *options) {
+  int opt = 0;
+  struct option long_option[] = {{"number", no_argument, NULL, 'n'},
+                                 {"squeeze-blank", no_argument, NULL, 's'},
+                                 {"number-nonblank", no_argument, NULL, 'b'},
+                                 {0, 0, 0, 0}};
+
+  while ((opt = getopt_long(argc, argv, "+bevnstTE", long_option, NULL)) !=
+         -1) {
+    switch (opt) {
+      case 'b':
+        options->b = 1;
+        break;
+
+      case 'e':
+        options->e = 1;
+        options->v = 1;
+        break;
+
+      case 'v':
+        options->v = 1;
+        break;
+
+      case 'n':
+        options->n = 1;
+        break;
+
+      case 's':
+        options->s = 1;
+        break;
+
+      case 't':
+        options->t = 1;
+        options->v = 1;
+        break;
+
+      case 'T':
+        options->t = 1;
+        break;
+
+      case 'E':
+        options->e = 1;
+        break;
+
+      default:
+        exit(1);
+    }
+  }
+}
+
+void numstr_n(int *strCount, int *flag_bn, int ch) {
+  if (*flag_bn == 1) {
+    printf("%6d\t", ++(*strCount));
+    *flag_bn = 0;
+  }
+  if(ch == '\n') {
+    *flag_bn = 1;
+  }
+}
+
+void numstr_b(int *strCount, int *flag_bn, int ch) {
+  
+  
+  if (*flag_bn == 1 && ch != '\n') {
+    printf("%6d\t", ++(*strCount));
+    *flag_bn = 0;
+  }
+
+  if (ch == '\n') {
+    *flag_bn = 1;
+  }
+}
+
+void numstr_s(int *flag_error, int *prev_newline, int *strCount, int *ch) {
+  if (*ch == '\n') {
+    (*strCount)++;
+
+    if (*prev_newline == 0) {
+      putchar(*ch);
+      *prev_newline = 1;
+      *strCount = 0;
+    }
+
+  } else {
+    if (*strCount >= 1) {
+      putchar('\n');
+      *strCount = 0;
+    }
+
+    putchar(*ch);
+    *prev_newline = 0;
+  }
+
+  if (*ch == EOF) {
+    *flag_error = 1;
+  }
+}
+
+void numstr_t(int *ch) {
+  if (*ch == 9) {
+    putchar('^');
+    *ch = 'I';
+  }
+}
+
+void numstr_v(int *ch) {
+  if (*ch != 9 && *ch < 32 && *ch != 10) {
+    *ch += 64;
+    putchar('^');
+  }
+  if (*ch == 127) {
+    putchar('^');
+    *ch -= 64;
+  }
+  if (*ch > 127) {
+    putchar('M');
+    putchar('-');
+    //printf("%d", *ch);
+    //putchar('^');
+    *ch -= 128;
+  }
+}
+
+void outline(char **file, opt options, int optind, int *strCount) {
+  int flag_bn = 1;
+  int prev_newline = 1;
+  int flag_error = 0;
+  
+  int ch = 0;
+
+  FILE *name = fopen(file[optind], "r");
+  if (name == NULL) {
+    perror("./s21_cat");
+  } else {
+    while ((ch = getc(name)) != EOF) {
+
+      if (options.e == 1 && ch == '\n') putchar('$');  // вывод $
+      if (options.n == 1 && !options.b)
+        numstr_n(strCount, &flag_bn, ch);  // вывод номера всех строк
+      if (options.b == 1 && !options.n)
+        numstr_b(strCount, &flag_bn, ch);  // вывод номера непустых строк
+      if (options.s == 1 && flag_error != 1)
+        numstr_s(&flag_error, &prev_newline, strCount,
+                 &ch);  // удаление пустых строк
+      if (options.t == 1) numstr_t(&ch);
+      if (options.v == 1) numstr_v(&ch);
+
+      if (ch != EOF && options.s != 1) {
+        putchar(ch);
+      }
+    }
+
+    fclose(name);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  opt options = {0};
+  parser(argc, argv, &options);
+
+  int strCount = 0;
+  
+  for (int i = optind; i < argc; i++) {
+    outline(argv, options, i, &strCount);
+  }
+
+  return 0;
+}
